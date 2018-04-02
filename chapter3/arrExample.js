fc3f987998dd9f37a4e30b6bf03b2c9bacfb2901
femtearenan <minerva@femtearenan.se>
2018-04-02 16:07:31 +0200

    added array length example

--- chapter3/arrExample.js
+++ chapter3/arrExample.js
@@ -1,3 +1,4 @@
 var flavors = ["vanilla", "butterscotch", "lavender", "chocolate", "cookie dough"];
 
 console.log("Today's flavor is " + flavors[3]);
+console.log("The length of the flavor array is " + flavors.length);
